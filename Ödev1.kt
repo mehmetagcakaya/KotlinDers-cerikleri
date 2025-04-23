9cbcd804dbd423d3c660ca8a06f6745a27f12090
Mehmet Ağcakaya <yedeklendiniz2@gmail.com>
2025-04-23 20:31:28 +0300

    ilk gün  dosyasını ekleme

---
+++ Ödev1.kt
@@ -0,0 +1,71 @@
+package com.mehmetagcakaya.kotlindersi1.degiskenler
+
+fun main() {
+    var sehir="Antalya"
+    var ulke = "Turkiye"
+    var telefon = "+1-212-111-2233"
+    var posta_kodu=38280
+    var email="deneme@gmail.com"
+    var meslek ="Muhendis"
+    var stok_miktarı=30
+    var musteri_adi="Mehmet"
+    var bakiye=1905
+    var dogum_gunu="13.01.2000"
+    var maas=105000
+    var medeni_durum="Bekar"
+    var urun_yorum="Harika bir urundu."
+    var odeme_tarihi="13.06.2024"
+    var odeme=3450
+    var siparis_adeti=10
+    var araba_modeli="Mercedes"
+    var kitap_adı="Demografik Isgal"
+    var yayınlama_tarihi="07.07.2007"
+    var indirim_miktarı=40
+    var oda_sayısı=60
+    var enlem=20.7
+    var boylam=36.4
+    var urun_adı="Canta"
+    var yemek_fiyatı=450
+    var marka="Nike"
+    var muzik_adi="Kesildi Fermanim"
+    var video_suresi=10
+    var urun_puanı=3.7
+    var resim_adı="Guzel Gunler"
+    var dosya_formatı=".png"
+    var renk="Sari"
+    var renk_kodu="#FFFF00"
+    var telefon_modeli="Samsung-M52"
+    var ekran_boyutu=7
+    var agırlık=1.1
+    var ulusal_gun="23.04.2025"
+    var tatil_gunu="01.05.2025"
+    var rezervasyon_tarihi="03.12.2022"
+    var sokak_adı="Oba"
+    var otobus_hattı="202B"
+    var kalan_dakika=7
+    var takip_kodu="192-A2"
+    var kupon_suresi=30
+    var kupon_kodu="3A025VB2AD"
+    var fatura_adresi="Kestel Mahallesi Universite Caddesi No:7"
+
+    println("Sehir:$sehir"+" "+ "Ulke:$ulke"+" "+"Telefon numarasi:$telefon"+" "+"Posta kodu:$posta_kodu")
+    println("Emailiniz:$email"+" "+"Mesleginiz:$meslek")
+    println("Urun stok miktari:$stok_miktarı "+" "+"Musteri adi:$musteri_adi"+" "+"Bakiyesi:$bakiye")
+    println("Musteri dogum tarihi:$dogum_gunu"+" "+"Maasi:$maas "+" "+"Medeni durumu:$medeni_durum"+" "+"Yorumu:$urun_yorum")
+    println("Odeme tarihi:$odeme_tarihi"+" "+"Odeme miktari:$odeme"+" "+"Siparis Adedi:$siparis_adeti")
+    println("Araba modeli:$araba_modeli")
+    println("Kitap adi: $kitap_adı"+" "+"Yayinlama tarihi:$yayınlama_tarihi"+" "+"Indirim miktari:$indirim_miktarı")
+    println("Oda sayisi:$oda_sayısı"+" "+"Bulundugu enlem:$enlem"+" "+"Bulundugu boylam:$boylam")
+    println("Urunun adi:$urun_adı"+" "+"Yenilen yemek fiyati:$yemek_fiyatı"+" "+" Urunun markasi:$marka")
+    println("Muzik adi:$muzik_adi"+" "+"Video suresi:$video_suresi"+" "+"Urun puani:$urun_puanı")
+    println("Resimin adi:$resim_adı"+" "+"Dosya formati:$dosya_formatı")
+    println("Rengi:$renk"+" "+"Renk kodu:$renk_kodu"+" "+"Telefon modeli:$telefon_modeli"+" "+"Ekran boyutu:$ekran_boyutu"+" "+"Agirligi:$agırlık")
+    println("Ulusal gun:$ulusal_gun"+" "+"Tatil gunu:$tatil_gunu"+" "+"Rezervasyon tarihi:$rezervasyon_tarihi")
+    println("Sokak adi:$sokak_adı"+" "+"Otobus hatti:$otobus_hattı"+" "+"Kalan dakika:$kalan_dakika"+" "+"Takip kodu:$takip_kodu")
+    println("Kupon suresi:$kupon_suresi"+" "+"Kupon kodu:$kupon_kodu"+" "+"Fatura adresi:$fatura_adresi")
+
+
+
+
+
+}
